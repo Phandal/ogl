5fb3a81be578088001b737c5d7ac873f4266e577
Phandal <bfeld.111@gmail.com>
2025-05-20 22:31:40 -0500

    added the log module for logging to stdout

--- src/log.c
+++ src/log.c
@@ -1,20 +1,73 @@
-#include <__stdarg_va_list.h>
-#include <cstdarg>
+#include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
 
 #include "log.h"
 
-static int LOG_FILE = STDERR_FILENO;
+static FILE *LOG_FILE = NULL;
 
-void log_error(const char *fmt, ...) {
-  log_message(LOG_ERROR, fmt, ...);
-}
+enum {
+  LOG_ERROR,
+  LOG_WARN,
+  LOG_INFO,
+  LOG_DEBUG,
+};
 
-void set_log_file(int file) { LOG_FILE = file; }
+void set_log_file(FILE *file) { LOG_FILE = file; }
 
 void log_message(int level, const char *fmt, ...) {
+  if (LOG_FILE == NULL) {
+    set_log_file(stderr);
+  }
+
+  switch (level) {
+  case LOG_ERROR:
+    fprintf(LOG_FILE, "[ERROR]: ");
+    break;
+  case LOG_WARN:
+    fprintf(LOG_FILE, "[WARN]: ");
+    break;
+  case LOG_INFO:
+    fprintf(LOG_FILE, "[INFO]: ");
+    break;
+  default:
+    fprintf(LOG_FILE, "[DEBUG]: ");
+    break;
+  }
+
   va_list args;
-  va_stat(args, fmt);
-  vfprintf(LOG_FILE, fmt, );
+  va_start(args, fmt);
+  vfprintf(LOG_FILE, fmt, args);
+  va_end(args);
+  fprintf(LOG_FILE, "\n");
 }
+
+void log_error(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  log_message(LOG_ERROR, fmt, args);
+  va_end(args);
+}
+
+void log_warn(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  log_message(LOG_WARN, fmt, args);
+  va_end(args);
+}
+
+void log_info(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  log_message(LOG_INFO, fmt, args);
+  va_end(args);
+}
+
+
+void log_debug(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  log_message(LOG_DEBUG, fmt, args);
+  va_end(args);
+  
+}
+
--- src/log.h
+++ src/log.h
@@ -1,19 +1,11 @@
 #ifndef LOG_H
 
 #define LOG_H
-
-enum {
-  LOG_ERROR,
-  LOG_WARN,
-  LOG_INFO,
-  LOG_DEBUG,
-};
+void log_set_file(int file);
 
 void log_error(const char *fmt, ...);
 void log_warn(const char *fmt, ...);
 void log_info(const char *fmt, ...);
 void log_debug(const char *fmt, ...);
-
-void log_set_file(int file);
 
 #endif // LOG_H
