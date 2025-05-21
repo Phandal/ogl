c032d695b231831f860d46466d785285f1537753
Phandal <bfeld.111@gmail.com>
2025-05-20 22:31:56 -0500

    Finished Hello-Window

--- src/window.c
+++ src/window.c
@@ -5,18 +5,66 @@
 
 #include "log.h"
 
+#define WIDTH 800
+#define HEIGHT 600
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  }
+}
+
 int main(void) {
-  // Initialize GLFW
-  glfwInit();
+  if (glfwInit() != GL_TRUE) {
+    log_error("could not initialize GLFW");
+    exit(-1);
+  }
+  log_info("GLFW initialized");
   
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // Set the major version
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6); // Set the minor version
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use Core Profile, which gives smaller version of opengl functionality
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Remove all features that are marked as depracated in the current version (Neede for Mac?)
 
-  GLFWwindow *window = glfwCreateWindow(600, 800, "Hello Triangle", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Hello Window", NULL, NULL);
   if (window == NULL) {
-    log_error("could not create GLFW window.");
+    log_error("could not create GLFW window");
+    glfwTerminate();
     exit(-1);
   }
+  log_info("created GLFW window");
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    log_error("could not initialize GLAD");
+    glfwTerminate();
+    exit(-1);
+  }
+  log_info("GLAD initialized");
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  log_info("set frame buffer size callback");
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  log_info("shutting down");
+  glfwDestroyWindow(window);
+  log_info("destroyed window");
+
+  glfwTerminate();
+  log_info("GLFW deinitialized");
+
+  return 0;
 }
