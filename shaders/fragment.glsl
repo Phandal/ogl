2225672a90a70b36b3acb360c331667cf49e191f
Phandal <bfeld.111@gmail.com>
2025-06-04 15:40:14 -0500

    smiley alpha channel working properly

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -8,5 +8,10 @@ uniform sampler2D containerTexture;
 uniform sampler2D smileyTexture;
 
 void main() {
- FragColor = mix(texture(containerTexture, TexCoord), texture(smileyTexture, TexCoord), 0.2) * vec4(myColor, 1.0);
+  vec4 smileyColor = texture(smileyTexture, TexCoord);
+  if (smileyColor.w < 0.1) {
+    FragColor = texture(containerTexture, TexCoord) * vec4(myColor, 1.0);
+  } else {
+    FragColor = mix(texture(containerTexture, TexCoord), smileyColor, 0.2) * vec4(myColor, 1.0);
+  }
 }
